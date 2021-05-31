4054d3a41ced8b3b4db6b2ff90898bdf98a3c255
Goy-Ell <egoyty@gmail.com>
2021-05-31 14:44:06 +0200

    creation branch participant

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParticipantController extends AbstractController
+{
+    /**
+     * @Route("/participant/recherche", name="participant_recherche")
+     */
+    public function recherche(): Response
+    {
+        return $this->render('participant/recherche.html.twig', [
+            'controller_name' => 'ParticipantController',
+        ]);
+    }
+}
